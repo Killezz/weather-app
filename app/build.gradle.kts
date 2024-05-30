b1d48a42021de2a8014d292fdb59e9499ea23a5a
Killezz <50575565+Killezz@users.noreply.github.com>
2024-05-30 16:24:02 +0300

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.weather.app"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
